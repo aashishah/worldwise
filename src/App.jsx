75475c95a14ee27967743b29d7e4a7b0c568c493
Aashi <aashishah0475@gmail.com>
2026-04-16 20:08:40 +0530

    set all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
         <Route path="app" element={<AppLayout />}></Route>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
