ee86ae7d5086cb0239f50d48d0be5d80233c38d6
Aashi <aashishah0475@gmail.com>
2026-04-17 19:47:12 +0530

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import City from "./components/City";
+import Form from "./components/Form";
 import { useState, useEffect } from "react";
 import CountryList from "./components/CountryList";
 
@@ -52,7 +53,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
